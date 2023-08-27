8c5188ebd88d8723fdd2b069437537e0ec4a177f
Muhmmad Awd <muhmmadawd@gmail.com>
2023-08-27 07:41:30 +0300

    init vans && vanDetail pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import "./style/App.css";
+import "./server";
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 
 export default function App() {
   return (
@@ -13,11 +16,14 @@ export default function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
