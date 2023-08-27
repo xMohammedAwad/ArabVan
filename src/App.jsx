7af98a31f37c650daef3dbf9ea2a80e9fd0b1bb0
Muhmmad Awd <muhmmadawd@gmail.com>
2023-08-27 06:57:25 +0300

    init about && Home pages

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./style/App.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
