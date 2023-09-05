dd2cc26935caf1c2372f925cb7638b11c0c40a5f
Muhmmad Awd <muhmmadawd@gmail.com>
2023-09-05 06:16:27 +0300

    Using unique identifying array keys #9

--- src/components/RenderRoutes.jsx
+++ src/components/RenderRoutes.jsx
@@ -1,9 +1,9 @@
 import { Route } from "react-router-dom";
 
 export default function RenderRoutes(routes) {
-  return routes.map((route, i) => (
+  return routes.map((route) => (
     <Route
-      key={i}
+      key={route.path}
       path={route.path}
       element={route.element ? <route.element /> : null}
     >
